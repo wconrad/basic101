3ff8004f28179f148a90ef022640e14acc2d1b9e
Wayne Conrad <wconrad@yagni.com>
2014-02-05 14:24:32 -0700

    refactoring

---
+++ binary_math_operation.rb
@@ -0,0 +1,22 @@
+require_relative 'wrap_value'
+
+module Basic
+
+  module BinaryMathOperation
+
+    include WrapValue
+
+    def operate(left, right, program)
+      left_value = left.eval(program).value
+      right_value = right.eval(program).value
+      result = do_math(left_value, right_value)
+      wrap_value(result)
+    end
+
+    def ==(other)
+      other.is_a?(self.class)
+    end
+
+  end
+
+end
--- multiply.rb
+++ multiply.rb
@@ -4,17 +4,10 @@ module Basic
 
   class Multiply
 
-    include WrapValue
+    include BinaryMathOperation
 
-    def operate(left, right, program)
-      left_value = left.eval(program).value
-      right_value = right.eval(program).value
-      result = left_value * right_value
-      wrap_value(result)
-    end
-
-    def ==(other)
-      other.is_a?(self.class)
+    def do_math(left, right)
+      result = left * right
     end
 
   end
