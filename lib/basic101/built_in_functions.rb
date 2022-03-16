463b698d62fe1212e40d30abbb0a6e8ce9dbd23b
Wayne Conrad <kf7qga@gmail.com>
2022-03-15 21:56:47 -0700

    Move function require statements to where they belong

--- lib/basic101/built_in_functions.rb
+++ lib/basic101/built_in_functions.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+require_relative 'abs_function'
+require_relative 'asc_function'
+require_relative 'chr_function'
+require_relative 'cos_function'
+require_relative 'exp_function'
+require_relative 'int_function'
+require_relative 'left_function'
+require_relative 'len_function'
+require_relative 'log_function'
+require_relative 'mid_function'
+require_relative 'right_function'
+require_relative 'rnd_function'
+require_relative 'sgn_function'
+require_relative 'sin_function'
+require_relative 'sqr_function'
+require_relative 'str_function'
+require_relative 'tab_function'
+require_relative 'tan_function'
+require_relative 'val_function'
+
 module Basic101
 
   module BuiltInFunctions
--- lib/basic101/functions.rb
+++ lib/basic101/functions.rb
@@ -1,18 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'abs_function'
-require_relative 'asc_function'
-require_relative 'chr_function'
-require_relative 'int_function'
-require_relative 'left_function'
-require_relative 'len_function'
-require_relative 'mid_function'
-require_relative 'right_function'
-require_relative 'rnd_function'
-require_relative 'str_function'
-require_relative 'tab_function'
-require_relative 'val_function'
-
 module Basic101
 
   class Functions
